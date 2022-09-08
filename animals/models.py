362a6fcd1b16df886d3191b8e4e61ade4f99752b
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-08 12:07:44 -0300

    Adc as classes user,  doacao e adoção

--- animals/models.py
+++ animals/models.py
@@ -10,10 +10,34 @@ class Animals(models.Model):
     name_animal = models.CharField(max_length=200, blank=True,null=True)
     color = models.CharField(max_length = 20)
     breed = models.CharField(max_length = 50)
-    birth_date = models.IntegerField()
+    birth_date = models.DateField(auto_now_add=False, null=False)
     adoption_date = models.DateTimeField(auto_now_add=True, null=True)
     health_condition = models.CharField(max_length = 200)
+    vacine_status = models.CharField(max_length=50, blank=True,null=True)
+    vermifugue_status = models.CharField(max_length=50, blank=True,null=True)
+    observations = models.CharField(max_length=250, blank=True,null=True)
     image = models.ImageField(upload_to= upload_image_animal, blank=True, null=True)
+
+class User(models.Model):
+    id_user = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=50, blank=True,null=True)
+    document_cpf = models.BigIntegerField (blank=True, null=True)
+    birth_date = models.DateField(auto_now_add=False, null=False)
+    adress = models.CharField(max_length=50, blank=True,null=True)
+    email = models.CharField(max_length=50, blank=True,null=True)
+    contact = models.CharField(max_length=50, blank=True,null=True)
+
+class Doacao(models.Model):
+    id_doacao = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id_user = models.UUIDField(blank=False,null=False,editable=False)
+    id_animal = models.UUIDField(blank=False,null=False,editable=False)
+
+class Adocao(models.Model):
+    id_adocao = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id_doacao = models.UUIDField(blank=False,null=False,editable=False)
+    id_user = models.UUIDField(blank=False,null=False,editable=False)
+
+
 
 
 

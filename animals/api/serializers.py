21aa3ab945e9d4a6158c0452138255b433c9335d
naiarar <rnaiara92@gmail.com>
2023-12-04 21:18:06 -0300

    del

--- animals/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from animals import models
-
-class AnimalsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Animals
-        fields = '__all__'
--- animals/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from animals.api import serializers
-from animals import models
-
-class AnimalsViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.AnimalsSerializer
-    queryset = models.Animals.objects.all()

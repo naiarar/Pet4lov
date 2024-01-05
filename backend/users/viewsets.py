b1cb08a8eba15003fe44680e9130ced914d3b68c
naiarar <rnaiara92@gmail.com>
2024-01-05 14:07:26 -0300

    alteracoes

--- backend/users/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from users import serializers
-from users import models
-
-class UsersViewsets(viewsets.ModelViewSet):
-    serializer_class = serializers.UsersSerializer
-    queryset = models.User.objects.all()

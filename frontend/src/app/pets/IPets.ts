296eefc496b8c5e230ec15360486ad6f2428d17c
naiarar <rnaiara92@gmail.com>
2024-02-21 11:41:50 -0300

    componente pets

---
+++ frontend/src/app/pets/IPets.ts
@@ -0,0 +1,20 @@
+import { IOngs } from "../ongs/IOngs";
+
+export interface IPets {
+    ong : string,
+    id_animal : number,
+    state : string,
+    city : string,
+    type : string,
+    name_animal : string,
+    color : string,
+    breed :  string,
+    birth_date : string,
+    adoption_date : string,
+    health_condition : string,
+    vacine_status :  string,
+    vermifugue_status :  string,
+    observations : string,
+    image : string,
+
+}

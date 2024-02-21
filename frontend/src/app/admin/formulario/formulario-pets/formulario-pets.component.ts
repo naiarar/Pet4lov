bf361f897162dbaf986e07ad3ef788d9c1ecba8b
naiarar <rnaiara92@gmail.com>
2024-02-21 11:39:48 -0300

    criei o formulario para cadastro de pets

---
+++ frontend/src/app/admin/formulario/formulario-pets/formulario-pets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-pets',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-pets.component.html',
+  styleUrl: './formulario-pets.component.scss'
+})
+export class FormularioPetsComponent {
+
+}

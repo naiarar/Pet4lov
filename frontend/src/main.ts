759d5e9e117e3df9278dfa81fa67d9bcf69ffc84
naiarar <rnaiara92@gmail.com>
2024-02-21 11:42:46 -0300

    main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,12 @@
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/routes';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { AppModule } from './app/app.module';
+import { environment } from './app/environment/environment';
 
-bootstrapApplication(AppComponent,
-  {
-    providers: [
-      provideProtractorTestingSupport(),
-      provideRouter(routeConfig)
-    ]
-  }
-).catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));

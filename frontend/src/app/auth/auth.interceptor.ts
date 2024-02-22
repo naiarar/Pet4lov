1d4c82cfab9e64f9f7b67cf87275f184e9673926
naiarar <rnaiara92@gmail.com>
2024-02-22 13:57:27 -0300

    ok

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -4,34 +4,25 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { environment } from '../environment/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
   constructor(public authService: AuthService) {}
+
   intercept(
     request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    if (this.authService.isLoggedIn) {
-      let newRequest = request.clone({
-        url: `${environment.apiUrl}${request.url}`,
-        setHeaders: {
-          Authorization: `Bearer ${this.authService.getToken()}`,
-          Accept: 'application/json'
-        },
-      });
-      return next.handle(newRequest);
-    }
-    let newRequest = request.clone({
-      url: `${environment.apiUrl}/${request.url}`,
-      setHeaders: {
-        Accept: 'application/json'
-      },
+    next: HttpHandler ): Observable<HttpEvent<any>> {
+    const authToken = this.authService.getToken();
+
+    const authReq = request.clone({
+      headers: request.headers.set('Authorization', `Bearer ${authToken}`)
     });
-    return next.handle(newRequest);
+
+    return next.handle(authReq);
   }
 }
+

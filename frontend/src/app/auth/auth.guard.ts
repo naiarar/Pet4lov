3b90cb135b3daff160f4cec6f08e9f9613a1e98c
naiarar <rnaiara92@gmail.com>
2024-02-22 13:55:53 -0300

    funcionando se estiver logado

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,26 +1,9 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { inject } from "@angular/core";
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
-
-  canActivate: CanActivateFn = (
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> => {
-    if (!this.authService.isLoggedIn) {
-      return this.router.createUrlTree(['/login']);
-    }
-
-    return true;
+export const AuthGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) => {
+    return inject(AuthService).canActivate();
   }
-}

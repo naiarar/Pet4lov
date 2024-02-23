7d6110eb690b9a86d71e28fa936b8c6be372cc65
naiarar <rnaiara92@gmail.com>
2024-02-23 13:39:05 -0300

    add urls api

--- backend/adocao_pets/urls.py
+++ backend/adocao_pets/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include('adocao.urls')),
 ]

42838eb623ac51998f5efb7f84df2ff890d3d6de
naiarar <rnaiara92@gmail.com>
2024-03-12 14:49:56 -0300

    tirei a obrigatoriedade data nasc

--- backend/adocao/models.py
+++ backend/adocao/models.py
@@ -13,7 +13,7 @@ class Usuario(AbstractUser):
     id_user = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100)
     document_cpf = models.BigIntegerField (blank=True, null=True)
-    birth_date = models.DateField(auto_now_add=False, null=False)
+    birth_date = models.DateField(auto_now_add=False, null=True)
     adress = models.CharField(max_length=50, blank=True,null=True)
     email = models.CharField(max_length=100, unique=True)
     contact = models.CharField(max_length=50, blank=True,null=True)

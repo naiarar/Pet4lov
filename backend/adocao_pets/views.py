75e4b56d013ae095c6e2d040492a3199cee3e252
naiarar <rnaiara92@gmail.com>
2024-03-15 18:59:47 -0300

    fazendo as validacoes

--- backend/adocao_pets/views.py
+++ backend/adocao_pets/views.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 # View para obter os tokens JWT
@@ -13,7 +14,7 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def protected_view(request):
-    # O usuário está autenticado e pode acessar esta view
-    # A lógica do view vai aqui
-    print("hello")
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
